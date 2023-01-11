81032cb23f6f70b06d3321660d6669772d6cec07
hyeongmin-park1 <lg3319@gmail.com>
2023-01-11 15:23:20 +0900

    with Router, Switch, Navlink

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home.js';
 import Profile from './Profile';
+import HistorySample from './HistorySample';
 
 const App = () => {
   return (
@@ -15,16 +16,27 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
         <li>
-          <Link to="/profile/gildong">gildong 프로필</Link>
+          <Link to="/history">History 예제</Link>
         </li>
       </ul>
       <hr />
+      <Switch>
       <Route path="/" component={Home} exact={true}/>
       <Route path="/about" component={About} />
       <Route path="/profile/:username" component={Profile} />
+      <Route path="/history" component={HistorySample}/>
+      <Route
+      render={({location})=>(
+        <div>
+          <h2>이 페이지는 존재하지 않습니다:</h2>
+          <p>{location.pathname}</p>
+        </div>
+      )}
+      />
+      </Switch>
     </div>
   );
 };
